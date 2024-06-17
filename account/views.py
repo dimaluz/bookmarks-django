9d0f2eafe37125a8f253a37cedb60c54d22876e3
dimaluz <dimaluz@rambler.ru>
2024-06-17 05:14:14 +0100

    Login and registration options were added

--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 def user_login(request):
     if request.method == "POST":
@@ -28,3 +28,19 @@ def user_login(request):
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section':'dashboard'})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            context = {'new_user':new_user}
+            return render(request, 'account/register_done.html', context) 
+    else:
+        user_form = UserRegistrationForm()
+    context = {'user_form':user_form}
+    return render(request, 'account/register.html', context)
+
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -117,3 +117,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'dashboard' 
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+
+PASSWORD_HASHERS = [ 
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher', 
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher', 
+    'django.contrib.auth.hashers.Argon2PasswordHasher', 
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher', 
+    'django.contrib.auth.hashers.ScryptPasswordHasher',
+]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
