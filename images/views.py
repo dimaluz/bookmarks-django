3aa1f3524e2d3b6bb7952461588718debdac6814
dimaluz <dimaluz@rambler.ru>
2024-06-23 13:25:56 +0100

    Like and Unlike option was added

--- images/views.py
+++ images/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 from .forms import ImageCreateForm
+from .models import Image
 
 @login_required
 def image_create(request):
@@ -19,3 +22,26 @@ def image_create(request):
         form = ImageCreateForm(data=request.GET)
     context = {'section':'images', 'form':form}
     return render(request, 'images/image/create.html', context)
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    context = {'section':'images', 'image':image}
+    return render(request, 'images/image/detail.html', context)
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status':'ok'})
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({'status':'error'})
