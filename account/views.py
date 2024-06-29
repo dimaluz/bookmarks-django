865fd832d48b89586d1ac4ec70e8734c47b56d95
dimaluz <dimaluz@rambler.ru>
2024-06-29 15:34:50 +0100

    User activites were added

--- account/views.py
+++ account/views.py
@@ -9,6 +9,8 @@ from django.views.decorators.http import require_POST
 
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile, Contact
+from actions.utils import create_action
+from actions.models import Action
 
 def user_login(request):
     if request.method == "POST":
@@ -32,7 +34,13 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section':'dashboard'})
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+    if following_ids:
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions.select_related('user', 'user__profile')[:10].prefetch_related('target')[:10]
+    context = {'section':'dashboard', 'actions':actions}
+    return render(request, 'account/dashboard.html', context)
 
 
 def register(request):
@@ -43,6 +51,7 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             context = {'new_user':new_user}
             return render(request, 'account/register_done.html', context) 
     else:
@@ -91,6 +100,7 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == 'follow':
                 Contact.objects.get_or_create(user_from=request.user, user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
             return JsonResponse({'status':'ok'})
